7fc84fc64e0d75ed48d379bbd49c2fffe8bab0c7
German0715 <germandariomorenojaimes@gmail.com>
2023-02-12 14:27:27 -0500

    Botones funcionando con estilo

--- equipo_front/src/views/vista2.vue
+++ equipo_front/src/views/vista2.vue
@@ -6,8 +6,8 @@
 <!-- Titulo -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Lato:ital@1&family=Roboto&display=swap" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css2?family=Lato:ital@1&family=Roboto&display=swap">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"  integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <HelloWorld class="tituloPri" msg="Estación meteorologica - UIS"/>
   <div class="Header">
     <img alt="estacion" class="imgPri" src="../imagenes_vista2/estacion.jpg">
